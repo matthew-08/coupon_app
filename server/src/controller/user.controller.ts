baf66ac728944832ca03ed3f73236a32c0bb4a89
Matthew <matthewjcrosby1@gmail.com>
2023-05-12 17:16:25 +0700

    sql queries createUser function

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express'
+import { appDatabase } from '..'
+import database from '../db/database'
 import { CreateUserInput } from '../schema/user.schema'
 
-const handleCreateUser = (
+const handleCreateUser = async (
     req: Request<{}, {}, CreateUserInput>,
     res: Response
 ) => {
     try {
+        const user = await appDatabase.users.createUser(req.body)
     } catch (error) {}
 }
+
+export { handleCreateUser }
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,14 @@
 import { Express } from 'express'
+import { handleCreateUser } from '../controller/user.controller'
+import validateSchema from '../middleware/validateSchema'
+import zUserSchema from '../schema/user.schema'
 
 const routes = (app: Express) => {
     app.get('/healthcheck', (req, res) => {
         res.sendStatus(200)
     })
     app.get('/api/coupons')
-    app.post('/api/user')
+    app.post('/api/user', validateSchema(zUserSchema), handleCreateUser)
 }
 
 export default routes
