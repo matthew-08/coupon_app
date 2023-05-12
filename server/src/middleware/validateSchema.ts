9ee16c5204777fb42dce2d0fd20444dae061722e
Matthew <matthewjcrosby1@gmail.com>
2023-05-12 17:06:24 +0700

    Created validation middleware

---
+++ server/src/middleware/validateSchema.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express'
+
+import z, { AnyZodObject } from 'zod'
+
+const validateSchema =
+    (schema: AnyZodObject) =>
+    (req: Request, res: Response, next: NextFunction) => {
+        try {
+            console.log(req.body)
+            schema.parse({
+                body: req.body,
+                query: req.query,
+                params: req.params,
+            })
+            next()
+        } catch (e: any) {
+            return res.status(400).send(e.errors)
+        }
+    }
+
+export default validateSchema
