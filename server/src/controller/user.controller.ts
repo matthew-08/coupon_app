f70c7e731f0fe24aa041bf93444059bcfe579aec
Matthew <matthewjcrosby1@gmail.com>
2023-05-12 17:09:51 +0700

    Create user controller

---
+++ server/src/controller/user.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express'
+import { CreateUserInput } from '../schema/user.schema'
+
+const handleCreateUser = (
+    req: Request<{}, {}, CreateUserInput>,
+    res: Response
+) => {
+    try {
+    } catch (error) {}
+}
