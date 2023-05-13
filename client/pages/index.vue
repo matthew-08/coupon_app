e9a07e17f74474f77b06b5d2bd2dc2aa10fd47d1
Matthew <matthewjcrosby1@gmail.com>
2023-05-13 14:56:26 +0700

    Create tailwind css config

---
+++ client/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <header
+    class="flex justify-center items-center p-5 px-20"
+    >
+        <h2
+        class="font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-cyan-500 to-cyan-300"
+        >
+            SuperCoupons
+        </h2>
+    </header>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
