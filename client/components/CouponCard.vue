9e47106e614e21d665d72169bc9f1c467503e12f
Matthew <matthewjcrosby1@gmail.com>
2023-05-14 14:48:02 +0700

    created modal for coupon clicks

---
+++ client/components/CouponCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    id="show-modal"
+    class="flex flex-col items-center p-4 shadow-lg border rounded-2xl"
+    @click="$emit('showModal')"
+  >
+    <FontAwesomeIcon
+      :icon="['fa-brands', 'playstation']"
+      size="5x"
+    />
+    <h2
+      class="font-bold font-sans mr-auto mb-1 mt-4"
+    >
+      25% Off
+    </h2>
+    <div
+      class="flex justify-between gap-3 text-sm"
+    >
+      <p class="text-cyan-500">
+        Playstation
+      </p>
+      <p
+        class="text-gray-500"
+      >
+        5/3 - 5/5
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+const emits = defineEmits(['showModal'])
+</script>
+
+<style scoped>
+
+</style>
--- client/pages/coupons.vue
+++ client/pages/coupons.vue
@@ -3,57 +3,30 @@
   <section
     class="grid grid-cols-2 gap-2" 
   >
-    <div
-      class="flex flex-col items-center p-4 shadow-lg border rounded-2xl"
-    >
-      <FontAwesomeIcon
-        :icon="['fa-brands', 'playstation']"
-        size="5x"
-      />
-      <h2
-        class="font-bold font-sans mr-auto mb-1 mt-4"
-      >
-        25% Off
-      </h2>
-      <div
-        class="flex justify-between gap-3 text-sm"
-      >
-        <p class="text-cyan-500">
-          Playstation
-        </p>
-        <p
-          class="text-gray-500"
-        >
-          5/3 - 5/5
-        </p>
-      </div>
-    </div>
-    <div
-      class="flex flex-col items-center p-4 shadow-lg border rounded-2xl"
-    >
-      <FontAwesomeIcon
-        :icon="['fa-brands', 'playstation']"
-        size="5x"
-      />
-      <h2
-        class="font-bold font-sans mr-auto mb-1 mt-4"
-      >
-        25% Off
-      </h2>
-      <div
-        class="flex justify-between gap-3 text-sm"
-      >
-        <p class="text-cyan-500">
-          Playstation
-        </p>
-        <p>5/3 - 5/5</p>
-      </div>
-    </div>
+    <CouponCard @show-modal="handleCardClick" />
   </section>
+  <Teleport to="body">
+    <CouponModal
+      :show="showModal"
+      :coupon-info="{
+        company: 'hello',
+        expDate: '1155',
+      }"
+      @close="showModal = false"
+    />
+  </Teleport>
 </template>
 
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { Teleport } from 'vue';
+import { ref } from 'vue'
+
+const showModal = ref(false)
+const handleCardClick = () => {
+  console.log('click')
+  return showModal.value = !showModal.value  
+}
 
 
 </script>
